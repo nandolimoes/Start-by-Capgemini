94332d8683322f4bcacb4856b06a3ec3db35c1bc
Fernando Oliveira Limões <nandolimoes@hotmail.com>
2022-08-20 20:02:24 -0300

    Adicionada a pasta Trabalhando com tipos e resolvendo alguns ex. de portugol

---
+++ 2. Lógica de Programação e Algoritmos (Marcelo Bernard)/2. Trabalhando com tipos/index.js
@@ -0,0 +1,63 @@
+// Aula 2 – Trabalhando com Tipos
+// Parte 1 
+// Fortemente tipadas x fracamente tipadas
+// Ex: Brinquedo para macacos para testar o QI e colocar as formas geométricas -> Fortemente tipada
+
+// Parte 2
+// Na divisão geralmente espera um número com virgula, no visualG  pede que as variáveis que forem divisíveis sejam declaradas como real
+// Literal ou caractere é a mesma coisa
+// Logico: booleano
+
+// Parte 3
+// Estruturas de condição no visualG
+// var
+//    nota1, nota2, nota3, media: real
+   
+
+// inicio
+// Seção de Comandos 
+// Escreval("Informe a nota 1: ")
+// leia(nota1)
+// Escreval("Informe a nota 2: ")
+// leia(nota2)
+// Escreval("Informe a nota 3: ")
+// leia(nota3)
+// media <- (nota1+nota2+nota3) / 3
+
+// se media < 7 entao
+//    Escreval("Aluno está de recuperação!")
+// senao
+//    Escreval ("Aluno aprovado, parabéns!")
+// fimse
+
+
+// fimalgoritmo
+
+// Parte 4
+// Se não se
+// Ponto flutuante = número com virgula
+// var
+// nota1, nota2, nota3, media: real
+// inicio
+// Seção de Comandos 
+// Escreval("Informe a nota 1: ")
+// leia(nota1)
+// Escreval("Informe a nota 2: ")
+// leia(nota2)
+// Escreval("Informe a nota 3: ")
+// leia(nota3)
+// media <- (nota1+nota2+nota3)/3
+// se media >= 7 entao
+// Escreval("Parabéns você passou! Sua média foi de ",media)
+// senao
+//      se media < 5 entao
+//         Escreval ("Você reprovou com a média: ",media)
+//      senao
+//           Escreval("Você está de repuração com a média: ",media)
+//      fimse
+// fimse
+
+// fimalgoritmo
+
+// Parte 5
+
