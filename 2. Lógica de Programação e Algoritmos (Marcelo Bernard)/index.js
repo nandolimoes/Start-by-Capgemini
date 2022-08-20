8a0e70fbf33e9017feebd3fa11267238feb08bd4
Fernando Oliveira Limões <nandolimoes@hotmail.com>
2022-08-20 15:26:43 -0300

    2. Lógica de Programação e Algoritmos

---
+++ 2. Lógica de Programação e Algoritmos (Marcelo Bernard)/index.js
@@ -0,0 +1,16 @@
+// Aula 1
+// Lógica de programação e algoritmo (Prof. Marcelo Bernard)
+// Computador = Todo dispositivo que tem entrada, processamento e saída
+// Ex: microondas, freezer, carro, website, computador, videogame
+// Ciência da computação aborda muito mais coisas do que lógica e algoritmos, transforma dados em informação
+// Está cada vez mais fácil programar, a 20 anos precisaria de 50 línguas enquanto hoje 2 linhas 
+// Com Arduino com 10 linhas faz um robô girar 360º
+// Não há mais sucesso em qualquer área sem tecnologia, pois é base para todas as outras.
+
+// Aula 2
+// Lógica de programação = Algoritmos
+// Algoritmos = receita
+// Ex: como fazer um Nescau
+// Vários parâmetros alteram como o programa funciona
+// Como funciona um programa? Através de linhas de códigos, ou linhas de comandos, que estarão em uma IDE (Ferramenta de desenvolvimento, que pode de um bloco de notas até IDEs mais complexas como Delphi, Eclipse, VS, etc...) no qual essas opções terão a opção de compilação que lê as linhas de código e traduz as linhas de código para uma linguagem que o computador entenda. Hoje em dia as empresas utilizam a programação de ALTO NÍVEL, sendo o alto nível não precisa se preocupar com o hardware, enquanto as de BAIXO NÍVEL está mais próximo da linguagem de máquina sendo necessário uma preocupação com o hardware (memória, placa mãe, processador, barramento, etc...), sendo necessário um conhecimento muito mais profundo. Empresas que utilizam linguagens de baixo nível geralmente estão ligadas quando deseja uma performance maior do hardware, como a indústria de programação de jogos.
+// Vamos utilizar o Portugol utilizando o VisualG. A programação começou com a linguagem Fortran na década de 50, depois o “lisp(?)”, depois o algol(década de 60), em seguinte Delphi, pascal, etc...
